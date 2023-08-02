823cda43c87ce0a3075ad1ee0b60b8c8791c7c69
CagiestSky99830 <120144559+CagiestSky99830@users.noreply.github.com>
2023-08-01 19:40:58 -0500

    Add files via upload

---
+++ ST-83-Boilerplate-master/screens/CreateStory.js
@@ -0,0 +1,248 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  TextInput,
+  Dimensions,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import DropDownPicker from "react-native-dropdown-picker";
+
+import * as Font from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+export default class CreateStory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      previewImage: "image_1",
+      dropdownHeight: 40,
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      let preview_images = {
+        image_1: require("../assets/story_image_1.png"),
+        image_2: require("../assets/story_image_2.png"),
+        image_3: require("../assets/story_image_3.png"),
+        image_4: require("../assets/story_image_4.png"),
+        image_5: require("../assets/story_image_5.png"),
+      };
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>New Story</Text>
+            </View>
+          </View>
+          <View style={styles.fieldsContainer}>
+            <Image
+              source={preview_images[this.state.previewImage]}
+              style={styles.previewImage}
+            ></Image>
+            <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+            <DropDownPicker
+                items={[
+                  { label: "Image 1", value: "image_1" },
+                  { label: "Image 2", value: "image_2" },
+                  { label: "Image 3", value: "image_3" },
+                  { label: "Image 4", value: "image_4" },
+                  { label: "Image 5", value: "image_5" },
+                ]}
+                defaultValue={this.state.previewImage}
+                open={this.state.dropdownHeight == 170 ? true : false}
+                onOpen={() => {
+                  this.setState({ dropdownHeight: 170 });
+                }}
+                onClose={() => {
+                  this.setState({ dropdownHeight: 40 });
+                }}
+                style={{
+                  backgroundColor: "transparent",
+                  borderWidth: 1,
+                  borderColor: "orange",
+                }}
+                textStyle={{
+                  color: this.state.dropdownHeight == 170 ? "orange" : "orange",
+                  fontFamily: "Bubblegum-Sans",
+                }}
+                onSelectItem={(item) =>
+                  this.setState({
+                    previewImage: item.value,
+                  })
+                }
+              />
+            </View>
+            <ScrollView>
+              <TextInput
+              style = {styles.inputFonts}
+              onChangeText = {(title)=>this.setState({title})}
+              placeHolder = {"title"}
+              placeHolderTextColor = "orange"/>
+              <TextInput
+              style = {[
+                styles.inputFont,styles.inputFontExtra,styles.inputTextBig,
+              ]}
+              onChangeText = {(description)=>this.setState({description})}
+              placeHolder = {"description"}
+              multiline = {true}
+              numberOfLines = {4}
+              placeHolderTextColor = "orange"/>
+              <TextInput
+              style = {[
+                styles.inputFont,styles.inputFontExtra,styles.inputTextBig,
+              ]}
+              onChangeText = {(story)=>this.setState({story})}
+              placeHolder = {"story"}
+              multiline = {true}
+              numberOfLines = {20}
+              placeHolderTextColor = "orange"/>
+              <TextInput
+              style = {[
+                styles.inputFont,styles.inputFontExtra,styles.inputTextBig,
+              ]}
+              onChangeText = {(moral)=>this.setState({moral})}
+              placeHolder = {"moral"}
+              multiline = {true}
+              numberOfLines = {4}
+              placeHolderTextColor = "orange"/>
+            </ScrollView>
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  fieldsContainer: {
+    flex: 0.85,
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain",
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20)
+  },
+  storyImage: {
+    resizeMode: "contain",
+    width: "95%",
+    alignSelf: "center",
+    height: RFValue(250)
+  },
+  titleContainer: {
+    paddingLeft: RFValue(20),
+    justifyContent: "center"
+  },
+  storyTitleText: {
+    fontSize: RFValue(25),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  storyAuthorText: {
+    fontSize: RFValue(18),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  descriptionText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 13,
+    color: "white",
+    paddingTop: RFValue(10)
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(10)
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    borderRadius: RFValue(30)
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5)
+  }
+});
